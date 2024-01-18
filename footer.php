734ef3d384cfbfaf43dd363c5629db348449f7c7
AlejandroSoPa <asoladopalomino.cf@iesesteveterradas.cat>
2024-01-18 20:14:17 +0100

    Commit footer + start index.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="contact-info">
+        <p>Contacto:</p>
+        <p>Javier Bernabeu - Correo electrónico: <a href="mailto:jbernabeucaballero.cf@iesesteveterradas.cat" target="_blank">jbernabeucaballero.cf@iesesteveterradas.cat</a></p>
+        <p>Erik Tamano - Correo electrónico: <a href="mailto:etamanosantos.cf@iesesteveterradas.cat" target="_blank">etamanosantos.cf@iesesteveterradas.cat</a></p>
+        <p>Alejandro Soldado - Correo electrónico: <a href="mailto:asoldadopalomino.cf@iesesteveterradas.cat" target="_blank">asoldadopalomino.cf@iesesteveterradas.cat</a></p>
+    </div>
+
+    <div class="links">
+        <a href="https://www.iesesteveterradas.cat">Institut Esteve Terrades</a>
+        <a href="https://github.com/ErikTamanoSantos/Proyecto_Vota">GitHub</a>
+    </div>
+</footer>
