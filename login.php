d84b95005ef0d61b53a9008a0ee26226bf5b9c67
Alejandro <95918617+AlejandroSoPa@users.noreply.github.com>
2024-01-22 16:06:12 +0100

    Merge from dev + Login

--- login.php
+++ login.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio de sesion</title>
 </head>
 <body>
-    
+    <h1>Inicia sesion</h1>
+    <form method="POST">
+        <input type="email" name="userEmail" placeholder="Email">
+        <input type="password" name="pwd" placeholder="Contrasenya">
+        <button type="submit">Enviar</button>
+    </form>
+
+    <?php
+        if(isset($_POST['userEmail']) && isset($_POST['pwd'])){
+            try {
+                $pwd = $_POST['pwd'];
+                $userEmail = $_POST['userEmail'];
+                $dsn = "mysql:host=localhost;dbname=project_vota";
+                $pdo = new PDO($dsn, 'root', 'Bbdd1nariO');
+                
+                $query = $pdo->prepare("SELECT Email FROM Users WHERE password = SHA2(:pwd, 512) AND Email = :userEmail");
+                $query->bindParam(':pwd', $pwd, PDO::PARAM_STR);
+                $query->bindParam(':userEmail', $userEmail, PDO::PARAM_STR);
+                $query->execute();
+                
+                $row = $query->fetch();
+                $correct = false;
+                while ($row) {
+                    $_SESSION["login"] = "correcto";
+                    header("Location:./index.php");
+                }
+                if (!$correct) {
+                    echo "Login incorrecto";
+                }
+
+            } catch (PDOException $e){
+                echo $e->getMessage();
+            }
+        }
+    ?>
+
 </body>
 </html>
