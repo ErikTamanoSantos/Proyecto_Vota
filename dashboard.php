9438dda0c1342cd8df25fb037ecefa527b475ec9
ErikTamano <72156568+ErikTamanoSantos@users.noreply.github.com>
2024-01-24 18:33:26 +0100

    Resolve confllicts

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,6 @@
 <?php
-    if (!isset($_SESSION['userID'])) {
+    session_start();
+    if (!isset($_SESSION['UserID'])) {
         include('./errors/error403.php');
     } else {
 ?><!DOCTYPE html>
